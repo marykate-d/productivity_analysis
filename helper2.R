7a18f1eac371fd5d722f2ad568031e0291a72db1
marykate-d <marykatedurka@gmail.com>
2025-10-01 10:28:44 -0700

    Add files via upload

---
+++ helper2.R
@@ -0,0 +1,325 @@
+
+library(tidyverse)
+library(tidymodels)
+library(skimr)
+library(recipes)
+library(themis)
+library(kknn)
+library(glmnet)
+library(discrim)
+library(DALEX)
+library(DALEXtra)
+
+employee <- read_csv("/Users/mkdurka/Documents/Behavioral Data Science II/Final Project/data/garments_worker_productivity.csv", show_col_types = FALSE)
+
+num <- employee %>% group_by(team) %>% summarize(n = n()) %>% arrange(n)
+
+
+employee <- employee %>% 
+  mutate(goal_met = ifelse(targeted_productivity <= actual_productivity, 1, 0))
+
+employee <- employee %>% 
+  select(!c("department", "date", "day", "actual_productivity")) %>% 
+  mutate(wip = ifelse(is.na(wip), 0, wip)) %>% 
+  mutate(quarter = as.factor(quarter),
+         team = as.factor(team),
+         goal_met = as.factor(goal_met))
+
+set.seed(123)
+split <- initial_split(employee, strata = goal_met)
+
+train <- training(split)
+test  <- testing(split)
+
+folds <- vfold_cv(train, v = 10, strata = goal_met)
+
+metrics_set <- metric_set(bal_accuracy, roc_auc)
+
+rec <- recipe(goal_met ~ ., data = train) %>% 
+  step_upsample(goal_met, over_ratio = 0.5) %>% 
+  step_normalize(all_numeric_predictors()) %>% 
+  step_dummy(all_factor_predictors()) 
+
+baked <- rec %>% 
+  prep() %>% 
+  bake(new_data = NULL)
+
+
+nb_spec <- naive_Bayes(mode = "classification",
+                       engine ="naivebayes") 
+
+nb_wflow <- workflow() %>% 
+  add_recipe(rec) %>% 
+  add_model(nb_spec)
+
+nb_fit <- nb_wflow %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+nb_metrics <- collect_metrics(nb_fit) %>% 
+  mutate(model = "Naive Bayes Model") %>% 
+  select(model, .metric, mean)
+
+knn_spec_tune <- nearest_neighbor(
+  mode = "classification",
+  engine = "kknn",
+  neighbors = tune()
+)
+
+knn_wf_tune <- workflow() %>% 
+  add_recipe(rec) %>% 
+  add_model(knn_spec_tune)
+
+neighbor_grid <- tibble(neighbors = seq(3, 20, 1))
+
+set.seed(123)
+
+knn_fit_tune <- knn_wf_tune %>% 
+  tune_grid(
+    resamples = folds,
+    grid = neighbor_grid,
+    metrics = metrics_set
+  )
+
+results <- collect_metrics(knn_fit_tune)
+
+results %>% 
+  ggplot(aes(x = neighbors, y = mean)) +
+  geom_line() +
+  theme_minimal()
+
+chosen_n <- knn_fit_tune %>% 
+  select_best(metric = "bal_accuracy")
+
+final_knn <- finalize_workflow(knn_wf_tune, chosen_n)
+
+knn_fit <- final_knn %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+knn_metrics <- collect_metrics(knn_fit) %>% 
+  mutate(model = "kNN Model") %>% 
+  select(model, .metric, mean)
+
+rf_spec <- rand_forest(mode = "classification") %>% 
+  set_engine("randomForest")
+
+rf_wflow <- workflow() %>% 
+  add_recipe(rec) %>% 
+  add_model(rf_spec)
+
+rf_fit <- rf_wflow %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+rf_metrics <- collect_metrics(rf_fit) %>% 
+  mutate(model = "Random Forest Model") %>% 
+  select(model, .metric, mean)
+
+boost_spec <- boost_tree(mode = "classification") %>% 
+  set_engine("xgboost")
+
+boost_wflow <- workflow() %>% 
+  add_recipe(rec) %>% 
+  add_model(boost_spec)
+
+boost_fit <- boost_wflow %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+boost_metrics <- collect_metrics(boost_fit) %>% 
+  mutate(model = "XGBoosted Tree Model") %>% 
+  select(model, .metric, mean)
+
+all_metrics <- bind_rows(nb_metrics, knn_metrics, rf_metrics, boost_metrics) %>% 
+  arrange(desc(mean)) %>%
+  pivot_wider(names_from = .metric, values_from = mean) %>% 
+  rename(Model = model,
+         `ROC AUC` = roc_auc,
+         `Balanced Accuracy` = bal_accuracy)
+
+xgb_spec <- boost_tree(
+  trees = tune(),
+  tree_depth = tune(),
+  learn_rate = tune(),
+  loss_reduction = tune(),  
+  sample_size = tune(),
+  mtry = tune()             
+) %>%
+  set_engine("xgboost") %>%
+  set_mode("classification")  
+
+workflow_obj <- workflow() %>%
+  add_model(xgb_spec) %>%
+  add_recipe(rec)
+
+xgb_grid <- grid_latin_hypercube(
+  trees(),
+  tree_depth(),
+  learn_rate(),
+  loss_reduction(),
+  sample_size = sample_prop(),
+  finalize(mtry(), train),
+  size = 20
+)
+
+set.seed(123)
+xgb_tuned <- tune_grid(
+  workflow_obj,
+  resamples = folds,
+  grid = xgb_grid,
+  metrics = metrics_set
+)
+
+best_params <- select_best(xgb_tuned)
+
+final_wf <- finalize_workflow(workflow_obj, best_params)
+
+b_final_fit <- final_wf %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+b_metrics <- collect_metrics(b_final_fit) %>% 
+  mutate(model = "XGBoosted Model") %>% 
+  select(model, .metric, mean)
+
+
+rf_spec_tune <- rand_forest(mode = "classification",
+                            mtry = tune(),
+                            min_n = tune(), 
+                            trees = tune()) %>% 
+  set_engine("randomForest")
+
+rf_wflow_tune <- workflow() %>% 
+  add_recipe(rec) %>% 
+  add_model(rf_spec_tune)
+
+rf_grid <- grid_regular(
+  mtry(range = c(2, 10)),
+  min_n(range = c(2, 10)),
+  trees(range = c(100, 1000)),
+  levels = 4  
+)
+
+set.seed(123)
+
+rf_tuned <- tune_grid(
+  rf_wflow_tune,
+  resamples = folds,
+  grid = rf_grid,
+  metrics = metrics_set
+)
+
+rf_best <- select_best(rf_tuned)
+
+rf_final_workflow <- finalize_workflow(rf_wflow_tune, rf_best)
+
+rf_final_fit <- rf_final_workflow %>% 
+  fit_resamples(folds,
+                metrics = metrics_set)
+
+rf_metrics <- collect_metrics(rf_final_fit) %>% 
+  mutate(model = "Random Forest Model") %>% 
+  select(model, .metric, mean)
+
+tuned_metrics <- bind_rows(rf_metrics, b_metrics) %>% 
+  pivot_wider(names_from = .metric,
+              values_from = mean)
+
+rf_test_fit <- rf_final_workflow %>% 
+  fit(test)
+
+bal_accuracy_metric <- rf_test_fit %>% 
+  augment(test) %>% 
+  bal_accuracy(goal_met, estimate = .pred_class)
+
+roc_auc_metric <- rf_test_fit %>% 
+  augment(test) %>% 
+  roc_auc(goal_met, .pred_0)
+
+metrics <- bind_rows(bal_accuracy_metric, roc_auc_metric)
+
+#code to load in helper functions below (may need to turn eval on to render?)
+source("/Users/mkdurka/Documents/Behavioral Data Science II/Final Project/helpers.R")
+
+
+explainer_rf <- make_explainer_obj(rf_test_fit)
+
+vip_res <- model_parts(explainer_rf)
+
+top_vars <- vip_res %>%
+  filter(variable != "_baseline_", variable != "_full_model_") %>%
+  group_by(variable) %>%
+  summarise(mean_loss = mean(dropout_loss), .groups = "drop_last") %>%
+  slice_max(mean_loss, n = 5) %>%
+  pull(variable)
+
+feature_plot <- ggplot_imp(vip_res %>% filter(variable %in% top_vars | variable == "_full_model_")) +
+  theme_minimal() +
+  labs(title = "Top 5 Most Important Features",
+       subtitle = "Tuned Random Forest Model") +
+  theme(plot.title = element_text(hjust = .5, size = 15),
+        plot.subtitle = element_text(hjust = .5, size = 12),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 12))
+
+pdp_incentive <- model_profile(explainer_rf, N = 100, variables = "incentive")
+
+incentive_plot <- ggplot_ice(pdp_incentive, incentive) +
+  theme_minimal() +
+  labs(
+    title = "Centered Conditional ICE Plot for Incentive",
+    x = "Incentive",
+    y = "yhat"
+  )
+
+
+pdp_workers <- model_profile(explainer_rf, N = 100, variables = "no_of_workers")
+
+worker_plot <- ggplot_ice(pdp_workers, no_of_workers) +
+  theme_minimal() +
+  labs(
+    title = "Centered Conditional ICE Plot for Number of Workers",
+    x = "Number of Workers",
+    y = "yhat"
+  ) 
+
+pdp_wip <- model_profile(explainer_rf, N = 100, variables = "wip")
+
+wip_plot <- ggplot_ice(pdp_wip, wip) +
+  theme_minimal() +
+  labs(
+    title = "Centered Conditional ICE Plot for Work in Progress ",
+    x = "Work in Progress",
+    y = "yhat"
+  ) 
+
+p <- pdp_2way(rf_test_fit, no_of_workers, wip)
+no_worker_2way <- p +
+  labs(
+    title = "2-way Partial Dependence Plot for Number of Workers and Work in Progress",
+    y = "Work in Progress",
+    fill = "Likelihood of Not Meeting Goal", 
+    x = "Number of Workers"
+  )
+
+pdp_smv <- model_profile(explainer_rf, N = 100, variables = "smv")
+
+smv_plot <- ggplot_ice(pdp_smv, smv) +
+  theme_minimal() +
+  labs(
+    title = "Centered Conditional ICE Plot for Standard Minute Value ",
+    x = "Standard Minute Value",
+    y = "yhat"
+  ) 
+
+pdp_team10 <- model_profile(explainer_rf, N = 100, variables = "team_X10")
+
+team_10_plot <- ggplot_ice(pdp_team10, team_X10) +
+  theme_minimal() +
+  labs(
+    title = "Centered Conditional ICE Plot for Team 10",
+    x = "Team 10",
+    y = "yhat"
+  ) 
+
---
+++ helpers.R
@@ -0,0 +1,156 @@
+
+make_explainer_obj <- function(fitted_workflow){
+  fitted_model <-
+    fitted_workflow %>% 
+    extract_fit_parsnip() # <- parsnip model_fit object
+  
+  feature_data <- 
+    fitted_workflow %>% 
+    extract_mold() %>% 
+    pluck("predictors") 
+  
+  outcome_data <- 
+    fitted_workflow %>% 
+    extract_mold() %>% 
+    pluck("outcomes") %>% 
+    pluck(1)    # <- is is a 1D df, make it a vector
+  
+  vip_features <- 
+    explain_tidymodels(
+      fitted_model, 
+      data = feature_data, 
+      y = as.numeric(as.character(outcome_data))
+    )
+  
+  return(vip_features)
+}
+
+
+ggplot_imp <- function(...) {
+  obj <- list(...)
+  metric_name <- attr(obj[[1]], "loss_name")
+  metric_lab <- paste(metric_name, 
+                      "after permutations\n(higher indicates more important)")
+  
+  full_vip <- bind_rows(obj) %>%
+    filter(variable != "_baseline_")
+  
+  perm_vals <- full_vip %>% 
+    filter(variable == "_full_model_") %>% 
+    group_by(label) %>% 
+    summarise(dropout_loss = mean(dropout_loss))
+  
+  p <- full_vip %>%
+    filter(variable != "_full_model_") %>% 
+    mutate(variable = fct_reorder(variable, dropout_loss)) %>%
+    group_by(variable) %>% 
+    mutate(
+      M = mean(dropout_loss),
+      min_loss = perm_vals %>% pull(dropout_loss)
+    ) %>% 
+    ggplot(aes(dropout_loss, variable)) 
+  if(length(obj) > 1) {
+    p <- p + 
+      facet_wrap(vars(label)) +
+      geom_segment(aes(xend = min_loss, x = M, yend = variable ), linewidth = 8, color = "dodgerblue2") +
+      geom_boxplot(aes(color = label, fill = label), alpha = 1, coef = 500, width = .33)
+  } else {
+    p <- p + 
+      geom_segment(aes(xend = min_loss, x = M, yend = variable ), linewidth = 8, color = "dodgerblue2") +
+      geom_boxplot(fill = "dodgerblue4", color = "dodgerblue4", alpha = 1, coef = 500, width = .33) +
+      scale_x_continuous(limits = c(perm_vals %>% pull(dropout_loss), NA))
+    
+  }
+  p +
+    theme(legend.position = "none") +
+    labs(x = metric_lab, 
+         y = NULL,  fill = NULL,  color = NULL)
+}
+
+ggplot_pdp <- function(obj, x) {
+  
+  p <- 
+    as_tibble(obj$agr_profiles) %>%
+    mutate(`_label_` = stringr::str_remove(`_label_`, "^[^_]*_")) %>% 
+    ggplot(aes(`_x_`, `_yhat_`)) +
+    geom_line(color = "midnightblue", linewidth = 1.2, alpha = 0.8)
+  
+  p
+}
+
+ggplot_ice <- function(obj, x, center = TRUE) {
+  agg_d <- as_tibble(obj$agr_profiles) %>%
+    mutate(`_label_` = stringr::str_remove(`_label_`, "^[^_]*_"))
+  
+  first_y <- agg_d %>% summarize(first_y = first(`_yhat_`)) %>% pull(first_y)
+  lines_data <- as_tibble(obj$cp_profiles) %>% 
+    group_by(`_ids_`) %>%
+    mutate(firsts = first(`_yhat_`))
+  if (center) {
+    agg_d <- mutate(agg_d, `_yhat_` = `_yhat_` - first_y)
+    lines_data <- lines_data %>% 
+      mutate(`_yhat_` = `_yhat_` - firsts)
+  }
+  
+  p <-
+    agg_d %>%
+    ggplot(aes(`_x_`, `_yhat_`)) +
+    geom_line(data = lines_data,
+              aes(x = {{ x }}, group = `_ids_`),
+              linewidth = 0.5, alpha = 0.1, color = "gray50")
+  
+  num_colors <- n_distinct(obj$agr_profiles$`_label_`)
+  
+  if (num_colors > 1) {
+    p <- p + geom_line(aes(color = `_label_`), linewidth = 1.2, alpha = 0.8)
+  } else {
+    p <- p + geom_line(color = "midnightblue", linewidth = 1.2, alpha = 0.8)
+  }
+  
+  return(p)
+}
+
+pdp_2way <- function(fitted_workflow, x1, x2, grid_steps = 64, N = 100, custom_range = NULL){
+  var1 <- enquo(x1)
+  x1_var_name <- quo_name(var1)
+  
+  var2 <- enquo(x2)
+  x2_var_name <- quo_name(var2)
+  
+  d <- fitted_workflow %>% 
+    extract_mold() %>% 
+    pluck("predictors")
+  
+  if (is.null(custom_range)){
+    grid <- crossing(
+      !! x1_var_name := seq(min(pull(d, !!var1)), max(pull(d, !!var1)), length.out = grid_steps),
+      !! x2_var_name := seq(min(pull(d, !!var2)), max(pull(d, !!var2)), length.out = grid_steps)
+    )
+  } else {
+    grid <- crossing(
+      !! x1_var_name := seq(custom_range[1,1], custom_range[2,1],  length.out = grid_steps),
+      !! x2_var_name := seq(custom_range[1,2], custom_range[2,2], length.out = grid_steps)
+    )
+  }
+  
+  
+  pred_df <-  d %>%
+    sample_n(N) %>%
+    mutate(.id = 1:n()) %>%
+    select(-c(!!var1, !!var2)) %>%
+    cross_join(grid)
+  
+  p <- extract_fit_parsnip(fitted_workflow) %>%
+    augment(pred_df) %>%
+    group_by(!!var1, !!var2) %>%
+    summarize(m = mean(.pred_0)) %>%
+    ggplot(aes(x = !!var1, y = !!var2, fill = m)) +
+    geom_raster(interpolate = TRUE) +
+    # geom_tile(linewidth=0.0) +
+    scale_fill_viridis_c() +
+    theme_minimal() +
+    theme(panel.grid = element_blank())
+  
+  return(p)
+  
+}
